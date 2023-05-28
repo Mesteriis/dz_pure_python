6772a7df8a38b2a1dae8e1e8d512646a683eb9de
alexander.mescheryakov <alexander.mescheryakov@semrush.com>
2023-05-28 12:42:27 +0200

    pre commit

--- lessons/dz6/main.py
+++ lessons/dz6/main.py
@@ -6,13 +6,13 @@ def main():
     points = 0
 
     for i in get_words():
-        print(f"Угадайте слово: {shuffle_words(i)}")
+        print(f"Угадайте слово: {shuffle_words(i)}")  # noqa: T201
         answer = input()
         if answer.strip() == i:
             points += 10
-            print("Верно! Вы получаете 10 очков.")
+            print("Верно! Вы получаете 10 очков.")  # noqa: T201
         else:
-            print(f"Неверно! Верный ответ – {i}.")
+            print(f"Неверно! Верный ответ – {i}.")  # noqa: T201
 
     save_history(name, points)
     show_stats()
